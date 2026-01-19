0f8a4966d768f148968217595dfc52afb563e284
priyanka <priyanka.yalamala1@gmail.com>
2026-01-19 09:15:27 +0100

    terraform infra

--- 30-sg-rules/locals.tf
+++ 30-sg-rules/locals.tf
@@ -1,5 +1,7 @@
 locals {
-    backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
+    #backend_alb_sg_id = data.aws_ssm_parameter.backend_alb_sg_id.value
     bastion_sg_id = data.aws_ssm_parameter.bastion_sg_id.value
+    mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
 }
+
 
--- 30-sg-rules/main.tf
+++ 30-sg-rules/main.tf
@@ -9,14 +9,14 @@
 # }
 
 
-resource "aws_security_group_rule" "backend_alb_bastion" {
-  type              = "ingress"
-  security_group_id = local.backend_alb_sg_id
-  source_security_group_id = local.bastion_sg_id
-  from_port         = 80
-  protocol          = "tcp"
-  to_port           = 80
-}
+# resource "aws_security_group_rule" "backend_alb_bastion" {
+#   type              = "ingress"
+#   security_group_id = local.backend_alb_sg_id
+#   source_security_group_id = local.bastion_sg_id
+#   from_port         = 80
+#   protocol          = "tcp"
+#   to_port           = 80
+# }
 
 resource "aws_security_group_rule" "bastion_laptop" {
   type              = "ingress"
@@ -27,3 +27,12 @@ resource "aws_security_group_rule" "bastion_laptop" {
   to_port           = 22
 }
 
+
+resource "aws_security_group_rule" "mongodb_bastion" {
+  type              = "ingress"
+  security_group_id = local.mongodb_sg_id
+  source_security_group_id = local.bastion_sg_id
+  from_port         = 22
+  protocol          = "tcp"
+  to_port           = 22
+}
