7a9da2523e7eb993426170f1a71cc6e2b5fd438b
priyanka <priyanka.yalamala1@gmail.com>
2026-01-20 13:13:41 +0100

    terraform infra

--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z0948150OFPSYTNVYZOY"
+}
+
+variable "domain_name" {
+    default = "daws86s.fun"
+}
