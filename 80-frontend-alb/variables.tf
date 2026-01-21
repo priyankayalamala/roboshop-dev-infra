777f9ca1140724ecf9d86254acbe6ace415d1f83
priyanka <priyanka.yalamala1@gmail.com>
2026-01-21 13:38:48 +0100

    terraform infra

---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z011939620HFSB9Q65KTW"
+}
+
+variable "domain_name" {
+    default = "priyankadevops.shop"
+}
